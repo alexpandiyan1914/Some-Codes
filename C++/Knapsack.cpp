f92988052d4d4a70a88119a8218771db4a3b9eed
Alexpandiyan <135411805+alexpandiyan1914@users.noreply.github.com>
2025-02-17 09:51:57 +0530

    Update C++

---
+++ C++/Knapsack.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int knapsack(int capacity, int weights[], int values[], int n) {
+    int dp[n + 1][capacity + 1];
+
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= capacity; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (weights[i - 1] <= w)
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    }
+
+    return dp[n][capacity];
+}
+
+int main() {
+    int n, capacity;
+    printf("Enter number of items: ");
+    scanf("%d", &n);
+
+    int weights[n], values[n];
+
+    printf("Enter weights and values of the items:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Item %d - Weight: ", i + 1);
+        scanf("%d", &weights[i]);
+        printf("Item %d - Value: ", i + 1);
+        scanf("%d", &values[i]);
+    }
+
+    printf("Enter knapsack capacity: ");
+    scanf("%d", &capacity);
+
+    int maxProfit = knapsack(capacity, weights, values, n);
+    printf("Maximum value that can be obtained: %d\n", maxProfit);
+
+    return 0;
+}
